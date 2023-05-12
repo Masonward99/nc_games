2cebb8f7cfcaaa536ce46dde7cad466bd9855194
Mason <mason@Masons-MacBook-Air-2.local>
2023-05-12 10:10:26 +0100

    hosting

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,15 @@ const ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
 
