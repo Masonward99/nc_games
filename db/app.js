33ef357663b9738fe4e599d403bbbda72d88471d
Mason <mason@Masons-MacBook-Air-2.local>
2023-06-03 17:02:28 +0100

    refactored to include routers

--- db/app.js
+++ db/app.js
@@ -1,38 +1,16 @@
 const express = require('express');
-const { getCategories } = require('../controllers/categories.controllers');
-const { getEndpoints } = require('../controllers/api.controllers');
-
-const {deleteCommentById, postCommentByReviewId }=require('../controllers/comments.controllers')
 
 const cors = require('cors')
 
-const { getReviewById, getCommentsByReview, getReviews, patchReview } = require('../controllers/reviews.controllers');
-
-const { getUsers } = require('../controllers/users.controllers');
-
+const apiRouter = require('../routes/api-router')
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/categories', getCategories);
+app.use('/api', apiRouter)
 
-app.get('/api', getEndpoints)
-
-app.get('/api/reviews/:review_id', getReviewById)
-
-app.post('/api/reviews/:review_id/comments',postCommentByReviewId)
-
-app.get('/api/reviews/:review_id/comments',getCommentsByReview)
-
-app.get('/api/reviews', getReviews)
-
-app.get('/api/users', getUsers)
-
-app.delete('/api/comments/:comment_id', deleteCommentById)
-
-app.patch('/api/reviews/:review_id',patchReview )
 
 app.use('*', ( req, res, next) => {
   res.status(404).send({ msg: "Endpoint not found!" });
