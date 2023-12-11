c93f84d64b3e6d8ce4551566784079fa67cd2fb0
Mason <mason@pc-79-13.customer.ask4.lan>
2023-12-11 06:30:23 +0000

    updated the json for all endpoints

--- routes/reviews-router.js
+++ routes/reviews-router.js
@@ -5,18 +5,18 @@ const reviewsRouter = require('express').Router()
 
 reviewsRouter
     .route('/:review_id')
-    .get(getReviewById)
-    .patch(patchReview)
-    .delete(deleteReviewById)
+        .get(getReviewById)
+        .patch(patchReview)
+        .delete(deleteReviewById)
 
 reviewsRouter
     .route('/:review_id/comments')
-    .get(getCommentsByReview)
-    .post(postCommentByReviewId)
+        .get(getCommentsByReview)
+        .post(postCommentByReviewId)
 
 reviewsRouter
     .route('/')
-    .get(getReviews)
-    .post(postReview)
+        .get(getReviews)
+        .post(postReview)
 
 module.exports = reviewsRouter
